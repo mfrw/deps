c908de4eb44b25d19857cae239dcc0dbc91117ba
Muhammad Falak Wani <mwani@microsoft.com>
2021-11-24 18:10:11 +0530

    pkg: graph: add fn to find cycle
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -66,4 +66,37 @@ func (g *Graph) GetAllEdgesChan() <-chan *Edge {
 	}()
 	return ch
 }
+
+func (g *Graph) FindCycle(u int) bool {
+	g.mu.RLock()
+	defer g.mu.RUnlock()
+
+	color := make([]uint8, len(g.AdjList), len(g.AdjList))
+	result := []int{}
+
+	cycleFound := false
+
+	var dfs func(u int)
+	dfs = func(u int) {
+		if color[u] == 2 {
+			return
+		}
+		color[u] = 1
+
+		for v := range g.AdjList[u] {
+			if color[v] == 0 {
+				dfs(v)
+			} else if color[v] == 1 {
+				cycleFound = true
+				return
+			}
+		}
+		result = append(result, u)
+		color[u] = 2
+
+	}
+
+	dfs(u)
+
+	return cycleFound
 }
