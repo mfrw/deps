5190ae86c87d30f4f869d671d92e61d9468d959f
Muhammad Falak Wani <mwani@microsoft.com>
2021-11-24 18:07:59 +0530

    pkg: graph: add RWMutex to make thread safe
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,17 +1,27 @@
 package graph
 
+import "sync"
+
 type Graph struct {
-	AdjList map[int][]int
+	mu      sync.RWMutex
+	AdjList map[int]map[int]struct{}
+}
+
+type Edge struct {
+	From int
+	To   int
 }
 
 func NewGraph() *Graph {
 	return &Graph{
-		AdjList: make(map[int][]int),
+		AdjList: make(map[int]map[int]struct{}),
 	}
 }
 
 func (g *Graph) AddNode(n int) {
-	g.AdjList[n] = make([]int, 0)
+	g.mu.Lock()
+	g.AdjList[n] = make(map[int]struct{})
+	g.mu.Unlock()
 }
 
 func (g *Graph) AddEdge(from, to int) {
@@ -21,5 +31,39 @@ func (g *Graph) AddEdge(from, to int) {
 	if _, ok := g.AdjList[to]; !ok {
 		g.AddNode(to)
 	}
-	g.AdjList[from] = append(g.AdjList[from], to)
+
+	g.mu.Lock()
+	g.AdjList[from][to] = struct{}{}
+	g.mu.Unlock()
+}
+
+func (g *Graph) GetAllEdgesFromChan(from int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		g.mu.RLock()
+		if vv, ok := g.AdjList[from]; ok {
+			for v := range vv {
+				ch <- v
+			}
+		}
+		close(ch)
+		g.mu.RUnlock()
+	}()
+	return ch
+}
+
+func (g *Graph) GetAllEdgesChan() <-chan *Edge {
+	ch := make(chan *Edge)
+	go func() {
+		g.mu.RLock()
+		for k, vv := range g.AdjList {
+			for v := range vv {
+				ch <- &Edge{k, v}
+			}
+		}
+		close(ch)
+		g.mu.RUnlock()
+	}()
+	return ch
+}
 }
